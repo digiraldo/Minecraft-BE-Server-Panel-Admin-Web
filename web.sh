43afebad506a07e07f3c496be3323251fc57e711
digiraldo <disaned@hotmail.com>
2020-12-16 22:53:52 -0500

    configuracion web.sd y panel.sh

--- web.sh
+++ web.sh
@@ -172,7 +172,24 @@ Print_Style "Reiniciando Servidor Web... " "$MAGENTA"
 sudo systemctl restart nginx
 echo "========================================================================="
 
+cd ~
+cd minecraftbe
+sudo rm -rf panel
+sudo rm -rf Minecraft-BE-Server-Panel-Admin-Web
+sudo git clone https://github.com/digiraldo/Minecraft-BE-Server-Panel-Admin-Web.git
+cd Minecraft-BE-Server-Panel-Admin-Web
 
+echo "======================================================================================="
+Print_Style "Creando directorio panel..." "$CYAN"
+sudo mv panel /home/usr/minecraftbe/
+sudo mv index.php /home/usr/minecraftbe/
+sudo mv location /home/usr/minecraftbe/
+sudo mv misitio.conf /home/usr/minecraftbe/
+sudo mv web.sh /home/usr/minecraftbe/
+cd ~
+cd minecraftbe
+sleep 4s
+sudo rm -rf Minecraft-BE-Server-Panel-Admin-Web
 
 
 
