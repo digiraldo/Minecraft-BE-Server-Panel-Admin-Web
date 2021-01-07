86fdb9bb544712a36985a3684173697e4495ed64
digiraldo <disaned@hotmail.com>
2021-01-07 16:48:59 -0500

    permisos

--- panel.sh
+++ panel.sh
@@ -209,9 +209,9 @@ sleep 2s
 echo "========================================================================="
 Print_Style "Configurando Permisos..." "$YELLOW"
 cd ~
-sudo chown -hR :www-data minecraftbe
-#sudo chown -hR www-data:www-data minecraftbe/panel
-#sudo chown -hR www-data:www-data minecraftbe/index.php
+sudo chown -h www-data minecraftbe
+sudo chown -hR www-data:www-data minecraftbe/panel
+sudo chown -hR www-data:www-data minecraftbe/index.php
 sudo sed -i '/www-data ALL=(ALL) NOPASSWD: ALL/d' /etc/sudoers
 sudo sed -i '$a www-data ALL=(ALL) NOPASSWD: ALL' /etc/sudoers
 sudo sed -n "/www-data ALL=(ALL) NOPASSWD: ALL/p" /etc/sudoers
