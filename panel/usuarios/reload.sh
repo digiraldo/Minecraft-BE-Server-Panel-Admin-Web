895e256126086479ce052a0fedeef011226257d8
disaned <you@example.com>
2021-06-19 16:25:34 -0500

    Configuracion permisos

--- panel/usuarios/reload.sh
+++ panel/usuarios/reload.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 #echo “Recargando Whitelist”
+sudo chown -hR username:www-data dirname/minecraftbe
+sudo chmod -R 775 dirname/minecraftbe
 screen -Rd servername -X stuff "whitelist reload$(printf '\r')"
-screen -Rd parceros -X stuff "say Hola Bienvenido al nuevo Jugador :')$(printf '\r')"
+screen -Rd servername -X stuff "say Hola Bienvenido al nuevo Jugador :')$(printf '\r')"
