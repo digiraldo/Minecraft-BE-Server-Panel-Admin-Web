b918bab6730fc28f364a11432953ef8f5e71c181
digiraldo <disaned@hotmail.com>
2023-12-11 16:46:18 -0500

    Cambio colores

--- prop.sh
+++ prop.sh
@@ -3,56 +3,7 @@
 # Instale las dependencias necesarias RClone y fuse para montar la nube
 echo "tutorial de instalacion en: https://gorobeta.blogspot.com"
 sleep 4s
-
-# Colores del terminal
-BLACK=$(tput setaf 0)
-RED=$(tput setaf 1)
-GREEN=$(tput setaf 2)
-YELLOW=$(tput setaf 3)
-LIME_YELLOW=$(tput setaf 190)
-BLUE=$(tput setaf 4)
-MAGENTA=$(tput setaf 5)
-CYAN=$(tput setaf 6)
-WHITE=$(tput setaf 7)
-BRIGHT=$(tput bold)
-NORMAL=$(tput sgr0)
-BLINK=$(tput blink)
-REVERSE=$(tput smso)
-UNDERLINE=$(tput smul)
-
-# Imprime una línea con color usando códigos de terminal
-Print_Style() {
-  printf "%s\n" "${2}$1${NORMAL}"
-}
-
-# Función para leer la entrada del usuario con un mensaje
-function read_with_prompt {
-  variable_name="$1"
-  prompt="$2"
-  default="${3-}"
-  unset $variable_name
-  while [[ ! -n ${!variable_name} ]]; do
-    read -p "$prompt: " $variable_name < /dev/tty
-    if [ ! -n "`which xargs`" ]; then
-      declare -g $variable_name=$(echo "${!variable_name}" | xargs)
-    fi
-    declare -g $variable_name=$(echo "${!variable_name}" | head -n1 | awk '{print $1;}')
-    if [[ -z ${!variable_name} ]] && [[ -n "$default" ]] ; then
-      declare -g $variable_name=$default
-    fi
-    echo -n "$prompt : ${!variable_name} -- aceptar? (y/n)"
-    read answer < /dev/tty
-    if [ "$answer" == "${answer#[Yy]}" ]; then
-      unset $variable_name
-    else
-      echo "$prompt: ${!variable_name}"
-    fi
-  done
-}
-
-
-
-Print_Style "Servidor: servername Configurado..." "$GREEN"
+echo -e "\e[32m Servidor: servername Configurado... \e[0m" #VERDE
 echo "========================================================================="
 sudo sed -n "/server-name=/p" server.properties | sed 's/server-name=/Nombre del Servidor: .... /'
 sudo sed -n "/level-name=/p" server.properties | sed 's/level-name=/Nombre del Nivel: ....... /'
