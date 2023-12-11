f6a543afadae9b29606f0fa100b51059361e1a79
digiraldo <disaned@hotmail.com>
2023-12-11 15:06:46 -0500

    Colores en BAsh

---
+++ color.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# 
+# https://soloconlinux.org.es/colores-en-bash/
+# https://soloconlinux.org.es/colores-en-bash/
+
+
+
+echo -e "\e[30m Texto en Color #30 NEGRO \e[0m"
+echo -e "\e[31m Texto en Color #31 ROJO \e[0m"
+echo -e "\e[32m Texto en Color #32 VERDE \e[0m"
+echo -e "\e[33m Texto en Color #33 AMARILLO \e[0m"
+echo -e "\e[34m Texto en Color #34 AZUL \e[0m"
+echo -e "\e[35m Texto en Color #35 MORADO \e[0m"
+echo -e "\e[36m Texto en Color #36 CIAN \e[0m"
+echo -e "\e[37m Texto en Color #37 GRIS \e[0m"
+
+
+echo -e "\e[1;32m Texto en Color + Negrilla \e[0m"
+echo -e "\e[3;32m Texto en Color + Cursiva \e[0m"
+echo -e "\e[4;32m Texto en Color + Subrayado \e[0m"
+echo -e "\e[5;32m Texto en Color + Parpadeo \e[0m"
+echo -e "\e[7;32m Texto en Color + Color Inverso \e[0m"
