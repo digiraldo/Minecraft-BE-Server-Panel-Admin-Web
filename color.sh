6257c025c4bf21c69ec11f67245bf532eeadc58d
digiraldo <disaned@hotmail.com>
2023-12-11 15:20:36 -0500

    colores

--- color.sh
+++ color.sh
@@ -3,8 +3,6 @@
 # https://soloconlinux.org.es/colores-en-bash/
 # https://soloconlinux.org.es/colores-en-bash/
 
-
-
 echo -e "\e[30m Texto en Color #30 NEGRO \e[0m"
 echo -e "\e[31m Texto en Color #31 ROJO \e[0m"
 echo -e "\e[32m Texto en Color #32 VERDE \e[0m"
@@ -24,12 +22,12 @@ echo -e "\e[7;32m Texto en Color + Color Inverso \e[0m"
 
 
 
-echo -e "\e[0m Texto en Color + Color Fondo NEGRO \e[40m"
-echo -e "\e[0m Texto en Color + Color Fondo ROJO \e[41m"
-echo -e "\e[0m Texto en Color + Color Fondo VERDE \e[42m"
-echo -e "\e[0m Texto en Color + Color Fondo AMARILLO \e[43m"
-echo -e "\e[0m Texto en Color + Color Fondo AZUL \e[44m"
-echo -e "\e[0m Texto en Color + Color Fondo MORADO \e[45m"
-echo -e "\e[0m Texto en Color + Color Fondo CIAN \e[46m"
-echo -e "\e[30m Texto en Color + Color Fondo GRIS \e[47m"
-echo -e "\e[30m Texto en Color + Color Fondo BLANCO \e[48m"
+echo -e "\e[1;37m Texto en Color + Color Fondo NEGRO \e[40m"
+echo -e "\e[1;37m Texto en Color + Color Fondo ROJO \e[41m"
+echo -e "\e[1;37m Texto en Color + Color Fondo VERDE \e[42m"
+echo -e "\e[1;37m Texto en Color + Color Fondo AMARILLO \e[43m"
+echo -e "\e[1;37m Texto en Color + Color Fondo AZUL \e[44m"
+echo -e "\e[1;37m Texto en Color + Color Fondo MORADO \e[45m"
+echo -e "\e[1;37m Texto en Color + Color Fondo CIAN \e[46m"
+echo -e "\e[1;30m Texto en Color + Color Fondo GRIS \e[47m"
+echo -e "\e[1;30m Texto en Color + Color Fondo BLANCO \e[48m"
