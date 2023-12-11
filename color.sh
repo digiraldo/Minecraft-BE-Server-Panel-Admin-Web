fe3db68765e49d394ca567ab5a63632b7c7e36c6
digiraldo <disaned@hotmail.com>
2023-12-11 15:49:06 -0500

    colores

--- color.sh
+++ color.sh
@@ -13,7 +13,9 @@ echo -e "\e[36m Texto en Color #36 CIAN \e[0m"
 echo -e "\e[37m Texto en Color #37 GRIS \e[0m"
 echo -e "\e[38m Texto en Color #38 BLANCO \e[0m"
 
+
 echo -e "\e[37m ======================================================================= \e[0m"
+
 
 echo -e "\e[1;38m Texto en Color + Negrilla \e[0m"
 echo -e "\e[3;38m Texto en Color + Cursiva \e[0m"
@@ -21,7 +23,9 @@ echo -e "\e[4;38m Texto en Color + Subrayado \e[0m"
 echo -e "\e[5;38m Texto en Color + Parpadeo \e[0m"
 echo -e "\e[7;38m Texto en Color + Color Inverso \e[0m"
 
+
 echo -e "\e[37m ======================================================================= \e[0m"
+
 
 echo -e "\e[1;38m Texto en Color + Color Fondo NEGRO \e[41m"
 echo -e "\e[1;38m Texto en Color + Color Fondo ROJO \e[42m"
@@ -33,16 +37,17 @@ echo -e "\e[1;38m Texto en Color + Color Fondo CIAN \e[47m"
 echo -e "\e[1;30m Texto en Color + Color Fondo GRIS \e[48m"
 echo -e "\e[1;30m Texto en Color + Color Fondo BLANCO \e[49m"
 
+
 echo -e "\e[37m ======================================================================= \e[0m"
+
 
 # Colores (Sin estilo)
 echo -e "\e[31m ROJO \e[33m  AMARILLO \e[32m VERDE \e[0m NORMAL"
-
 # Negrita + Colores
 echo -e "\e[1;31m ROJO \e[1;33m  AMARILLO \e[1;32m VERDE \e[1;39m NEGRITA \e[0m"
-
 # Tachado + Colores
 echo -e "\e[9;31m ROJO \e[9;33m  AMARILLO \e[9;32m VERDE \e[9;39m TACHADO \e[0m"
+
 
 echo -e "\e[37m ======================================================================= \e[0m"
 
