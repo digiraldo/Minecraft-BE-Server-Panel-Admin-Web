2c003d508627b1b8de797303f702a5a626155724
digiraldo <disaned@hotmail.com>
2023-12-21 18:00:06 -0500

    habilitar funcion read_with_prompt

--- panel.sh
+++ panel.sh
@@ -5,6 +5,31 @@
 # Repositorio de GitHub: https://github.com/digiraldo/Minecraft-BE-Server-Panel-Admin-Web
 # Crear Usuario https://noviello.it/es/como-crear-un-usuario-de-sudo-en-linux-debian/
 
+# Function to read input from user with a prompt
+function read_with_prompt {
+  variable_name="$1"
+  prompt="$2"
+  default="${3-}"
+  unset $variable_name
+  while [[ ! -n ${!variable_name} ]]; do
+    read -p "$prompt: " $variable_name </dev/tty
+    if [ ! -n "$(which xargs)" ]; then
+      declare -g $variable_name=$(echo "${!variable_name}" | xargs)
+    fi
+    declare -g $variable_name=$(echo "${!variable_name}" | head -n1 | awk '{print $1;}' | tr -cd '[a-zA-Z0-9]._-')
+    if [[ -z ${!variable_name} ]] && [[ -n "$default" ]]; then
+      declare -g $variable_name=$default
+    fi
+    echo -n "$prompt : ${!variable_name} -- accept (y/n)?"
+    read answer </dev/tty
+    if [[ "$answer" == "${answer#[Yy]}" ]]; then
+      unset $variable_name
+    else
+      echo "$prompt: ${!variable_name}"
+    fi
+  done
+}
+
  # sudo systemctl start $ServerName.service
 
 cd ~
